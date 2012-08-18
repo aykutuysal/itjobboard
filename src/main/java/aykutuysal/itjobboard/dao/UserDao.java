51dcb8df79a209b17280b3b0a554a86e95bbe054
aykutuysal <aykutuysal@gmail.com>
2012-08-18 18:09:54 +0300

    cleaned

--- src/main/java/aykutuysal/itjobboard/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package aykutuysal.itjobboard.dao;
-
-import aykutuysal.itjobboard.model.User;
-
-public interface UserDao extends GenericDao<User, Long> {
-
-	public User getByEmail(String email);
-	
-}
