7b903f50025e8d35254fafc9b482758fbefcffba
aykutuysal <aykutuysal@gmail.com>
2012-08-18 18:02:27 +0300

    cleaned

--- README.md
+++
@@ -1,4 +0,0 @@
-itjobboard
-==========
-
-job portalspring 3.1, hibernate 4.1, spring security
--- src/main/java/aykutuysal/itjobboard/model/constant/ListingType.java
+++
@@ -1,8 +0,0 @@
-package aykutuysal.itjobboard.model.constant;
-
-public class ListingType {
-
-	public static final int PERMANENT = 0;
-	public static final int CONTRACT  = 1;
-	
-}
